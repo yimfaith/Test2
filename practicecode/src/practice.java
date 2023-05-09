d3438112f861fc7eb531edb615b8e0a3ba006245
usernameyimfaith <fyim192@gmail.com>
2023-05-09 13:47:48 -0700

    Tested out a Java file

---
+++ practicecode/src/practice.java
@@ -0,0 +1,6 @@
+public class practice {
+    public static void main(String[] args) {
+        System.out.println("This is a practice file.");
+        System.out.println("Hi \nI'm \nFaith");
+    }
+}
